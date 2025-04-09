27f7d8c18487ed5602397d246ec5fe50f80b21c5
Edinsoncg <edinsonck@gmail.com>
2025-04-09 15:44:51 -0500

    Actualización general en los archivos de la base de datos y creación de una carpeta contrato con sus diferentes partes del contrato para cada vista

--- BASE DE DATOS/tables.sql
+++ BASE DE DATOS/tables.sql
@@ -60,10 +60,10 @@ CREATE TABLE modality (
 	CONSTRAINT idx_modality_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE classroom_type (
+CREATE TABLE class_type (
 	id BIGINT UNSIGNED auto_increment NOT NULL,
 	`type` VARCHAR(255) NOT NULL,
-	CONSTRAINT idx_classroom_type_pk PRIMARY KEY (id)
+	CONSTRAINT idx_class_type_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE house (
@@ -77,6 +77,12 @@ CREATE TABLE permission (
 	id BIGINT UNSIGNED auto_increment NOT NULL,
 	name varchar(255) NOT NULL,
 	CONSTRAINT idx_permission_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE status (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	name varchar(255) NOT NULL,
+	CONSTRAINT idx_status_pk PRIMARY KEY (id)
 );
 
 /*DEPENDIENTES*/
@@ -110,8 +116,10 @@ CREATE TABLE student (
 	id BIGINT UNSIGNED auto_increment NOT NULL,
 	user_id BIGINT UNSIGNED NOT NULL,
 	student_code BIGINT UNSIGNED NOT NULL,
+	status_id BIGINT UNSIGNED NOT NULL, 
 	CONSTRAINT idx_student_pk PRIMARY KEY (id),
-	CONSTRAINT idx_student_user_id_FK FOREIGN KEY (user_id) REFERENCES lingui_academy.`user`(id)
+	CONSTRAINT idx_student_user_id_FK FOREIGN KEY (user_id) REFERENCES lingui_academy.`user`(id),
+	CONSTRAINT idx_student_status_id_FK FOREIGN KEY (status_id) REFERENCES lingui_academy.`status`(id)	
 );
 
 CREATE TABLE student_contract (
@@ -143,47 +151,14 @@ CREATE TABLE teacher_user_language (
 	CONSTRAINT idx_teacher_user_language_language_id_FK FOREIGN KEY (language_id) REFERENCES lingui_academy.`language`(id)
 );
 
-CREATE TABLE classroom_information (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	classroom_id BIGINT UNSIGNED NOT NULL,
-	modality_id BIGINT UNSIGNED NOT NULL,
-	level_id BIGINT UNSIGNED NOT NULL,
-	unit_id BIGINT UNSIGNED NOT NULL,
-	component_id BIGINT UNSIGNED DEFAULT NULL NULL,
-	teacher_user_language_id BIGINT UNSIGNED NOT NULL,
-	classroom_type_id BIGINT UNSIGNED NOT NULL,
-	start_date_time DATETIME NOT NULL,
-	end_date_time DATETIME NOT NULL,
-	duration_hour BIGINT UNSIGNED NOT NULL,
-	CONSTRAINT idx_classroom_information_pk PRIMARY KEY (id),
-	CONSTRAINT idx_classroom_information_classroom_id_FK FOREIGN KEY (classroom_id) REFERENCES lingui_academy.classroom(id),
-	CONSTRAINT idx_classroom_information_modality_id_FK FOREIGN KEY (modality_id) REFERENCES lingui_academy.modality(id),
-	CONSTRAINT idx_classroom_information_level_id_FK FOREIGN KEY (level_id) REFERENCES lingui_academy.`level`(id),
-	CONSTRAINT idx_classroom_information_unit_id_FK FOREIGN KEY (unit_id) REFERENCES lingui_academy.unit(id),
-	CONSTRAINT idx_classroom_information_component_id_FK FOREIGN KEY (component_id) REFERENCES lingui_academy.component(id),
-	CONSTRAINT idx_classroom_information_classroom_type_id_FK FOREIGN KEY (classroom_type_id) REFERENCES lingui_academy.classroom_type(id),
-	CONSTRAINT idx_classroom_information_teacher_user_language_id_FK FOREIGN KEY (teacher_user_language_id) REFERENCES lingui_academy.teacher_user_language(id)
-);
-
-CREATE TABLE student_classroom_information (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	student_id BIGINT UNSIGNED NOT NULL,
-	classroom_information_id BIGINT UNSIGNED NOT NULL,
-	CONSTRAINT idx_student_classroom_information_pk PRIMARY KEY (id),
-	CONSTRAINT idx_student_classroom_information_student_id_FK FOREIGN KEY (student_id) REFERENCES lingui_academy.student(id),
-	CONSTRAINT idx_student_classroom_information_classroom_information_id_FK FOREIGN KEY (classroom_information_id) REFERENCES lingui_academy.classroom_information(id)
-);
-
 CREATE TABLE enrollment_level (
 	id BIGINT UNSIGNED auto_increment NOT NULL,
-	student_id BIGINT UNSIGNED NOT NULL,
-	language_id BIGINT UNSIGNED NOT NULL,
+	student_language_id BIGINT UNSIGNED NOT NULL,
 	level_id BIGINT UNSIGNED NOT NULL,
 	start_date DATE NOT NULL,
 	end_date DATE DEFAULT NULL NULL,
 	CONSTRAINT idx_enrollment_level_pk PRIMARY KEY (id),
-	CONSTRAINT idx_enrollment_level_student_id_FK FOREIGN KEY (student_id) REFERENCES lingui_academy.student(id),
-	CONSTRAINT idx_enrollment_level_language_id_FK FOREIGN KEY (language_id) REFERENCES lingui_academy.`language`(id),
+	CONSTRAINT idx_enrollment_level_student_language_id_FK FOREIGN KEY (student_language_id) REFERENCES lingui_academy.student_language(id),
 	CONSTRAINT idx_enrollment_level_level_id_FK FOREIGN KEY (level_id) REFERENCES lingui_academy.`level`(id)
 );
 
@@ -235,4 +210,35 @@ CREATE TABLE classroom (
 	house_id BIGINT UNSIGNED NOT NULL,
 	CONSTRAINT idx_classroom_pk PRIMARY KEY (id),
 	CONSTRAINT idx_classroom_house_id_FK FOREIGN KEY (house_id) REFERENCES lingui_academy.house(id)
+);
+
+CREATE TABLE classroom_session (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	classroom_id BIGINT UNSIGNED NOT NULL,
+	modality_id BIGINT UNSIGNED NOT NULL,
+	level_id BIGINT UNSIGNED NOT NULL,
+	unit_id BIGINT UNSIGNED NOT NULL,
+	component_id BIGINT UNSIGNED DEFAULT NULL NULL,
+	teacher_user_language_id BIGINT UNSIGNED NOT NULL,
+	class_type_id BIGINT UNSIGNED NOT NULL,
+	start_date_time DATETIME NOT NULL,
+	end_date_time DATETIME NOT NULL,
+	duration_hour BIGINT UNSIGNED NOT NULL,
+	CONSTRAINT idx_classroom_session_pk PRIMARY KEY (id),
+	CONSTRAINT idx_classroom_session_classroom_id_FK FOREIGN KEY (classroom_id) REFERENCES lingui_academy.classroom(id),
+	CONSTRAINT idx_classroom_session_modality_id_FK FOREIGN KEY (modality_id) REFERENCES lingui_academy.modality(id),
+	CONSTRAINT idx_classroom_session_level_id_FK FOREIGN KEY (level_id) REFERENCES lingui_academy.`level`(id),
+	CONSTRAINT idx_classroom_session_unit_id_FK FOREIGN KEY (unit_id) REFERENCES lingui_academy.unit(id),
+	CONSTRAINT idx_classroom_session_component_id_FK FOREIGN KEY (component_id) REFERENCES lingui_academy.component(id),
+	CONSTRAINT idx_classroom_session_class_type_id_FK FOREIGN KEY (class_type_id) REFERENCES lingui_academy.class_type(id),
+	CONSTRAINT idx_classroom_session_teacher_user_language_id_FK FOREIGN KEY (teacher_user_language_id) REFERENCES lingui_academy.teacher_user_language(id)
+);	
+
+CREATE TABLE student_attendance (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	student_id BIGINT UNSIGNED NOT NULL,
+	classroom_session_id BIGINT UNSIGNED NOT NULL,
+	CONSTRAINT idx_student_attendance_pk PRIMARY KEY (id),
+	CONSTRAINT idx_student_attendance_student_id_FK FOREIGN KEY (student_id) REFERENCES lingui_academy.student(id),
+	CONSTRAINT idx_student_attendance_classroom_information_id_FK FOREIGN KEY (classroom_session_id) REFERENCES lingui_academy.classroom_session(id)
 );
